24f0034c15ffcae159614548de96272aa26b1b0c
Saheem Iqbal <Siqbal@spartaglobal.com>
2019-04-17 14:32:09 +0100

    Initial commit of project structure

---
+++ src/main/java/com/sparta/create_Json_example/App.java
@@ -0,0 +1,15 @@
+package com.sparta.create_Json_example;
+
+
+import org.json.simple.JSONObject;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JSONObject userDetails = new JSONObject();
+        System.out.println(userDetails.getClass());
+
+    }
+}
---
+++ src/test/java/com/sparta/create_Json_example/AppTest.java
@@ -0,0 +1,20 @@
+package com.sparta.create_Json_example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
